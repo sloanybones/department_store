09a7de279efda24772c1e491085d2615c3175b23
Andrew Sloan <sloancodes@gmail.com>
2020-11-18 21:15:46 -0700

    finished Crud actions for model

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,13 +1,50 @@
 class DepartmentsController < ApplicationController
+  before_action :set_departments,  only: [:show , :edit, :update, :destroy] 
+  
   def index
+  @departments = Department.all
   end
 
   def show
   end
 
   def new
+    @department = Department.new
+    render partial: "form"
   end
 
   def edit
+    render partial: "form"
   end
+
+  def create
+    @department = Department.new(dep_params)
+  end
+
+  def update
+  if @department.save
+    redirect_to departments_path
+  else
+    render :new
+  end
+
+  def destroy
+      @department.destroy
+      redirect_to departments_path
+    end
+  end
+  
+  
+  private
+
+  def set_departments
+  @department = Department.find(params[:id])
+  end
+
+
+  def dep_params
+    params.require(:department).permit(:name)
+  end
+
 end
+
