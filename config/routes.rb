547bc05d32a4e5a24c9690aad5084aa7b8812142
Andrew Sloan <sloancodes@gmail.com>
2020-11-18 22:04:36 -0700

    add 2nd model crud ations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'items/index'
+  get 'items/show'
+  get 'items/new'
+  get 'items/edit'
  root "departments#index"
  
- resources :departments
+ resources :departments do
+  resources :items
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+30.times do
+    department.item.create(name: Faker::FunnyName.name_with_initial, price: Faker::Currency.symbol, Faker::Number.decimal  , body: Faker::Marketing.buzzwords )
+  end
+end
